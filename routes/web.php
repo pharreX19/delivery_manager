3f61eb6833269cc020d66d10141953e8a31fd072
affaz <affaz.a@hotmail.com>
2020-11-05 07:28:36 +0500

    created main front end redirect route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/{any}', function(){
+    return	view(public_path('front_end/index.html'));
+   })->where('any','.*');
+   
